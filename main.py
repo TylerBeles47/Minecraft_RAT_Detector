9c28b5f5e11b0447c032779b757c24df072d911d
TylerBeles47 <jazperjay47>
2025-04-30 19:40:38 -0500

    Added frontend vercel

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_origins=["https://security-frontend-sand.vercel.app"],
 )
 
 @app.get("/")
